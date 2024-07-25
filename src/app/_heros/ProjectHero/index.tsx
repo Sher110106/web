b3c85fd9dd45847f137271be20a0a91e0ef6f5a2
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2024-07-25 20:22:12 +0530

    Initial commit

---
+++ src/app/_heros/ProjectHero/index.tsx
@@ -0,0 +1,48 @@
+import React, { Fragment } from 'react'
+import Link from 'next/link'
+
+import { Project } from '../../../payload/payload-types'
+import { Gutter } from '../../_components/Gutter'
+import { Media } from '../../_components/Media'
+import RichText from '../../_components/RichText'
+import { formatDateTime } from '../../_utilities/formatDateTime'
+
+import classes from './index.module.scss'
+
+export const ProjectHero: React.FC<{
+  project: Project
+}> = ({ project }) => {
+  const { id, title, categories, meta: { image: metaImage, description } = {}, createdAt } = project
+
+  return (
+    <Fragment>
+      <Gutter className={classes.projectHero}>
+        <div className={classes.content}>
+          <div className={classes.leader}>
+            <div className={classes.categories}>
+              <div></div>
+            </div>
+          </div>
+          <h1 className={classes.title}>{title}</h1>
+          <div>
+            <p className={classes.description}>
+              {`${description ? `${description} ` : ''} `}
+              {'.'}
+            </p>
+          </div>
+        </div>
+        <div className={classes.media}>
+          <div className={classes.mediaWrapper}>
+            {!metaImage && <div className={classes.placeholder}>No image</div>}
+            {metaImage && typeof metaImage !== 'string' && (
+              <Media imgClassName={classes.image} resource={metaImage} fill />
+            )}
+          </div>
+          {metaImage && typeof metaImage !== 'string' && metaImage?.caption && (
+            <RichText content={metaImage.caption} className={classes.caption} />
+          )}
+        </div>
+      </Gutter>
+    </Fragment>
+  )
+}
